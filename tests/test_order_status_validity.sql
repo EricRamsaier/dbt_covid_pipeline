150b090f733589e61433e2c69e7be8b3b23174a6
Eric Ramsaier <eramsaier@gmail.com>
2025-05-04 17:58:33 +0000

    Cleanup of migrated files from older git repo in order to create a clean base.

--- tests/test_order_status_validity.sql
+++
@@ -1,9 +0,0 @@
-/*
-Test: Valid order_status values in fct_faker_orders
-Purpose: Ensures all order_status values are valid.
-*/
-
-SELECT 
-    order_status
-FROM {{ ref('fct_faker_orders') }}
-WHERE order_status NOT IN ('recent', 'old', 'very_old')

aad9ef9ba8af3506a98b2b4805a737ba085dc780
Eric Ramsaier <eramsaier@gmail.com>
2025-05-28 15:06:38 +0000

    chore(models): extract dim_owid_covid_location config and docs into properties YAML

--- models/marts/owid/dim_owid_covid_location.sql
+++ models/marts/owid/dim_owid_covid_location.sql
@@ -1,18 +1,3 @@
--- Created:       2024-05-10
--- Last Modified: 2025-05-23
--- Creator:       Eric Ramsaier
--- Model:         dim_owid_iso_code
--- Purpose:       Provides a unique list of OWID ISO codes with associated location and continent
--- Notes:
---   - Only includes ISO codes that appear in the source data
-
-
-{{
-  config(
-    unique_key='sk_owid_iso_code'
-  )
-}}
-
 WITH 
 int_data AS (
     SELECT * FROM {{ ref('int_owid_covid') }}
